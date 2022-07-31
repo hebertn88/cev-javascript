1c22044e663e314b6b49ac89d2ae3e889d081632
Hebert do Nascimento <hebertn88@gmail.com>
2022-07-31 16:29:16 -0300

    final

--- aula16/ex015/script.js
+++ aula16/ex015/script.js
@@ -9,13 +9,6 @@ let btnRein = document.getElementById('btnRein')
 let dResp = document.getElementById('dResp')
 
 //totalizadores
-/*
-qtd de itens
-val maximo
-val minimo
-soma itens
-media itens
-*/
 const numeros = {
     itens: [],
     qtd: function () {
@@ -48,20 +41,76 @@ const numeros = {
     },
     media: function () {
         return this.soma() / this.itens.length
-    }
+    },
 }
 
 btnAd.addEventListener('click', adNum)
 function adNum(){
-    numeros.itens.push(Number(iNum.value))
-    iNum.value = null
-    console.log(numeros.itens)
+    let num = Number(iNum.value)
+    //verifica se valor foi informado e se e valido
+    if (num.length == 0 || num < 1 || num > 100) {
+        alert('Informe um número válido!')
+    //verifica se valor ja existe
+    } else if (numeros.itens.indexOf(num) != -1) {
+        alert('Número adicionado anteriormente!')
+    } else {
+        numeros.itens.push(num) //adiciona item
+
+        let op = document.createElement('option')
+        op.text = `Valor ${num} adicionado.`
+        listNum.appendChild(op)
+
+        if (listNum.hidden) {
+            listNum.hidden = false
+        }
+
+        if (btnFin.disabled) {
+            btnFin.disabled = false
+        }
+
+        if (btnRein.disabled) {
+            btnRein.disabled = false
+        }
+
+        iNum.value = ''
+    }
 
 }
+
 btnFin.addEventListener('click', max)
 function max() {
-    console.log('max: ' + numeros.max())
-    console.log('min: ' + numeros.min())
-    console.log('soma: ' + numeros.soma())
-    console.log('media: ' + numeros.media())
+    //cria elementos
+    let pLen = document.createElement('p')
+    let pMax = document.createElement('p')
+    let pMin = document.createElement('p')
+    let pSoma = document.createElement('p')
+    let pMed = document.createElement('p')
+
+    //insere mensagem aos elementos
+    pLen.innerHTML = `Ao todo temos ${numeros.itens.length} números cadastrados.`
+    pMax.innerHTML = `O maior valor informado foi o ${numeros.max()}.`
+    pMin.innerHTML = `O menor valor informado foi o ${numeros.min()}.`
+    pSoma.innerHTML = `Somando todos os valores, temos ${numeros.soma()}.`
+    pMed.innerHTML = `A média dos valores digitados é ${numeros.media()}.`
+
+    //adiciona elementos a pagina
+    dResp.appendChild(pLen)
+    dResp.appendChild(pMax)
+    dResp.appendChild(pMin)
+    dResp.appendChild(pSoma)
+    dResp.appendChild(pMed)
+
+    //torna visivel
+    dResp.hidden = false
+}
+
+btnRein.addEventListener('click', reinicia)
+function reinicia() {
+    numeros.itens = [] //limpa numeros.itens
+    dResp.hidden = true //esconde dResp
+    dResp.innerHTML = '' //limpa dResp
+    listNum.hidden = true //esconde listNum
+    listNum.innerHTML = '' //limpa listNum
+    btnFin.disabled = true //desabilita btnFin
+    btnRein.disabled = true //desabilita btnRein
 }
